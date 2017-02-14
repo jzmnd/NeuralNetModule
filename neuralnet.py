9bc41f4b963dc0aa6f84a27ffb77e0403031e480
Jeremy Smith <j-smith@eecs.berkeley.edu>
2017-02-14 15:40:35 -0800

    Implementing general NN

--- neuralnet.py
+++ neuralnet.py
@@ -30,62 +30,78 @@ class FeedForwardNN():
 		self.name = name
 		self.path = os.path.dirname(os.path.abspath(__file__))
 
+		# Loads NN architecture and sorts layers by id number
 		self.architecture, self.nlayers = load_arch(architecture)
 		self.archname = self.architecture['name']
 		self.layers = sorted(self.architecture['layers'], key=lambda l: l['id'])
 
-	def forward(self, X, W):
+		# Holds cached values for each layer
+		self.cache = []
+
+		if not weights:
+			initW()
+
+	def initW(self):
+		"""Init weights with random numbers"""
+		for layer in self.layers:
+
+
+
+	def forward(self, X):
+		"""Forward propagation of data through NN"""
 		for layer in self.layers:
 			if layer['type'] == 'inputLayer':
-				continue
+				self.cache.append(X)
+
 			if layer['type'] == 'hiddenLayer':
-				acticvation_function = functions[layer['config']['activation']]
-				Wi = W[layer['id'] - 1]
-				X = activation_fuction(np.dot(Wi, X))
+				activation_function = functions[layer['config']['activation']]
+				Wi = self.weights[layer['id'] - 1]
+
+				X = activation_function(np.dot(Wi, X))
+				self.cache.append(X)
+
 			if layer['type'] == 'outputLayer':
-				Wi = W[layer['id'] - 1]
+				Wi = self.weights[layer['id'] - 1]
+
 				X = np.dot(Wi, X)
+				self.cache.append(X)
+
 		return X
 
-
-	def backprop(self, X, W):
+	def backprop(self, dX):
+		"""Backward propagation of gradients through NN"""
+		dW_list = []
 		for layer in self.layer[::-1]:
 			if layer['type'] == 'outputLayer':
+				Xi = self.cache[layer['id']]
+				Wi = self.weights[layer['id'] - 1]
 				
-		return
+				dX_tmp = np.dot(Wi.T, dX)
+				dW = np.dot(dX, Xi.T)
+
+				dX = dX_tmp
+				dW_list.append(dW)
+
+			if layer['type'] == 'hiddenLayer':
+				activation_function_grad = gradientfunctions[layer['config']['activation']]
+				Xi = self.cache[layer['id']]
+				Wi = self.weights[layer['id'] - 1]
+
+				dX = activation_function_grad(Xi) * dX
+
+				dX_tmp = np.dot(Wi.T, dX)
+				dW = np.dot(dX, Xi.T)
+
+				dX = dX_tmp
+				dW_list.append(dW)
+
+			if layer['type'] == 'inputLayer':
+				continue
+
+		return dW_list[::-1]
 
 
 	def compute_loss(self):
 		return
 
-	def param_update(self):
-		return
 
-
-
-	def run(self):
-		return
-
-
-
-	def plot(self):
-		return
-
-
-
-	def writeout(self):
-		return
-
-
-
-
-
-
-# class RecursiveNN():
-# 	"""Class for descibing recursive neural network"""
-# 	def __init__(self, weights, bias):
-
-
-# class ConvNN():
-# 	"""Class for descibing convolutional neural network"""
-# 	def __init__(self, weights, bias):
