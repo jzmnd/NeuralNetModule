887e43e27ad74f0d6b4f19212d70a3841ca5e61c
Jeremy Smith <j-smith@eecs.berkeley.edu>
2017-02-17 16:16:34 -0800

    Added to nnsolver class

--- nnsolver.py
+++ nnsolver.py
@@ -17,6 +17,7 @@ from lossfunctionlib import *
 from graddescentlib import *
 from datautils import *
 from funcutils import *
+import yaml
 
 __author__ = "Jeremy Smith"
 __version__ = "1.0"
@@ -24,15 +25,16 @@ __version__ = "1.0"
 
 class Solver():
 	"""Class containing solver for training of NN using gradient descent"""
-	def __init__(self, model, data_training, data_val):
+	def __init__(self, model, data_training, data_val, k=1, config="default.config.yml"):
 		self.model = model
 		self.X_train = data_training[0]
 		self.y_train = data_training[1]
 		self.X_val = data_val[0]
 		self.y_val = data_val[1]
+		self.k = k
 
-		self.num_images_train = y_train.size
-		self.num_images_val = y_val.size
+		self.num_images_train = self.y_train.size
+		self.num_images_val = self.y_val.size
 		self.count = 1
 
 		self.gradchecks = []
@@ -40,5 +42,39 @@ class Solver():
 		self.accuracies_train = []
 		self.accuracies_val = []
 
-	def train(self):
+		self.config = load_config(config)
 
+		self.filenametag = "_{:s}_{:.0e}_{:.4f}_{:s}_{:s}".format(
+			self.model.name,
+			self.config['learning_rate'],
+			self.config['mu'],
+			self.config['btype'],
+			self.config['update_type'])
+
+		self.update_function = methods[self.config['update_type']]
+
+	def train(self):
+		print "TRAINING ON DATA"
+		print "  model name         :", self.model.name
+		print "  num of points      :", self.num_images_train
+		print "  num of dimensions  :", self.X_train.shape[0]
+		print "  num of classes     :", self.k
+		print "  solver update type :", self.config['update_type'], self.config['btype']
+		print "  init learning rate :", self.config['learning_rate']
+
+		self.init_weights(self.config['aini'], self.config['bini'])
+
+		# TODO: perform grad descent and updates
+
+		return
+
+	def init_weights(self, a, b):
+		if not self.model.weights:
+			self.model.weights = []
+			for layer in self.model.layers:
+				if layer['type'] == 'inputLayer':
+					d = layer['config']['dims']
+				else:
+					self.model.weights.append(initializeweights(self.num_images_train, d, k=layer['config']['dims'], a=a, b=b))
+					d = layer['config']['dims']
+		return
